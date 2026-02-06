12f791a066282f600434ba5ca94f2ebac87ed536
Sreeharsh Rajan <sreeharshkrajan@gmail.com>
2026-02-07 01:13:33 +0530

    chore: added Laravel Sanctum package for api authentication

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class AdminUserSeeder extends Seeder
@@ -12,13 +13,14 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password123'),
-            'email_verified_at' => now(),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+         User::updateOrCreate(
+            ['email' => env('ADMIN_EMAIL', 'admin@example.com')],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make(env('ADMIN_PASSWORD', 'password123')),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
